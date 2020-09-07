a2dc8492f24594a705705bd7f538420ae8438bf2
Smith Surasmith <ssurasmith+dev@gmail.com>
2020-09-07 16:45:26 -0700

    use new create-react-app

--- src/App.js
+++ src/App.js
@@ -1,14 +1,26 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import Nameplate from './Nameplate';
+import logo from './logo.svg';
 import './App.css';
 
-class App extends React.Component {
-    render() {
-        return (
-            <Nameplate title="Daily News"/>
-        );
-    }
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
 export default App;
